0ef77eb722060dcf4d5becd6ce163289b2087910
phlioni <ph.lioni@hotmail.com>
2021-12-02 09:08:21 -0300

    init

---
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using ProEventos.API.Models;
+
+
+namespace ProEventos.API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class EventoController : ControllerBase
+    {
+        public EventoController()
+        {
+            
+        }
+          
+
+        [HttpGet]
+        public IEnumerable<Evento> Get()
+        {
+            return new Evento(){
+                EventoId = 1,
+                Tema = "Pedro",
+                Local = "Angular",
+                Lote = "1",
+                QtdPessoas = 250,
+                DataEvento = "hoje"
+            };
+        }
+
+        [HttpGet("{id}")]
+        public IEnumerable<Evento[]> GetById(int id){
+
+        }
+    }
+}
